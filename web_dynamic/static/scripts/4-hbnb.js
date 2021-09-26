b06b2571b73d9d78d6da91d74cf1a3c4adcac528
SrDri <juanjcarabali@gmail.com>
2021-09-26 14:44:47 +0700

    Update and fix localhost

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -40,17 +40,17 @@ $(document).ready(function () {
   $('.filters > button').click(function () {
     $('.places > article').remove();
     $.ajax({
-	  type: 'POST',
-	  url: 'http://0.0.0.0:5001/api/v1/places_search',
-	  data: JSON.stringify({ amenities: Object.keys(amenities) }),
-	  dataType: 'json',
-	  contentType: 'application/json',
-	  success: function (data) {
+      type: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
         for (let i = 0; i < data.length; i++) {
-		  const place = data[i];
-		  $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+          const place = data[i];
+          $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
         }
-	  }
+      }
     });
   });
 });
